40bb81d19eeeedbe504ae0606cfbca6d54873b9b
Chengliang Cai <dev.chua@gmail.com>
2021-05-06 17:02:49 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'hhhhustlehard',
+  author: 'chua',
+  email: 'caicl34@163.com',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US',
